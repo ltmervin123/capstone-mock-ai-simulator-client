2451799eab05234de56dbf401ea2a7bfda10e177
ltmervin123 <ltmervin123@gmail.com>
2025-06-19 12:06:50 +0800

    feat: add LoginPage component and update routing; create Header layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        {/* Add more routes as needed */}
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../layouts/Header';
+
+export default function LoginPage() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
