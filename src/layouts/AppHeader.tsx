266807d7c324dfa2c17bbbd2644391e1826734bc
ltmervin123 <ltmervin123@gmail.com>
2025-10-26 20:45:01 +0800

    feat: refactor AppHeader to use authStore for user data

--- src/layouts/AppHeader.tsx
+++ src/layouts/AppHeader.tsx
@@ -1,10 +1,10 @@
 import { Link } from 'react-router-dom';
+import authStore from '@/stores/auth-store';
 
 export default function AppHeader() {
-  const user = {
-    name: 'Alvincent F. Sangco',
-    initials: 'AS',
-  };
+  const user = authStore((state) => state.user);
+  const name = `${user?.firstName} ${user?.lastName}`;
+  const initials = `${user?.firstName.charAt(0).toUpperCase()}${user?.lastName.charAt(0).toUpperCase()}`;
 
   return (
     <div className="relative z-50">
@@ -19,10 +19,10 @@ export default function AppHeader() {
           </Link>
           <div className="flex items-center gap-4">
             <span className="hidden text-sm font-medium text-white sm:block sm:text-base">
-              {user.name}
+              {name}
             </span>
             <div className="flex h-10 w-10 items-center justify-center rounded-full bg-white">
-              <span className="text-base font-bold sm:text-lg">{user.initials}</span>
+              <span className="text-base font-bold sm:text-lg">{initials}</span>
             </div>
           </div>
         </div>
